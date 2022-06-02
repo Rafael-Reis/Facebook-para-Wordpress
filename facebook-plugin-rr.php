4afd23ca6cca483bd44bb819941ea6d1196136f3
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2022-06-02 00:02:27 -0400

    refactor: modificaçãos de instalação

--- facebook-plugin-rr.php
+++ facebook-plugin-rr.php
@@ -2,18 +2,18 @@
 
 /*
  * Plugin Name: Facebook Plugin
- * Plugin URI: https://github.com/Rafael-Reis/facebook-plugin-rr
- * Description: Plugin de Integração de plugins do Facebook com themas do Wordpress, Ex: like box, comentários, botão curtir, botão compartilar etc.
+ * Plugin URI: https://github.com/rafaelreis96/facebook-plugin-rr
+ * Description: Plugin de Integração de plugins do Facebook com themas do Wordpress, Ex: like box, comentários, botão curtir, botão compartilar.
  * Version:     1.0
- * Author:      rafaelreis.eti.br
- * Author URI:  http://rafaelreis.eti.br
+ * Author:      rafaelreis.dev
+ * Author URI:  http://rafaelreis.dev
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: rr_fb
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	die( 'Invalid request.' );
+    die( 'Invalid request.' );
 }
 
 define('RR_PLUGIN', plugin_basename(__FILE__));
@@ -21,7 +21,7 @@ define('RR_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('RR_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 require plugin_dir_path(__FILE__) . 'includes/functions.php';
-require plugin_dir_path(__FILE__) . 'includes/options.php';
+require plugin_dir_path(__FILE__) . 'includes/classes/class-option.php';
 
 //Widgets
 require plugin_dir_path(__FILE__) . 'public/class-widget-botoes.php';
