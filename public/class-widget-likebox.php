e44749e25a70a0dd1d3734a86a310c60e58291d4
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2022-06-01 23:59:58 -0400

    refactor: melhoria recupearar dados para os widgets

--- public/class-widget-likebox.php
+++ public/class-widget-likebox.php
@@ -15,26 +15,25 @@ class widget_LikeBox extends WP_Widget {
 
     public function widget($argumentos, $instancia) {
         extract($argumentos);
-        $title  = strip_tags($instancia['title']);
+        $title = strip_tags($instancia['title']);
+        $data = RR_Option::get_instance()->get_values();
         
         echo $before_widget;
     ?>
     <div>
-
-
         <?= !empty($title) ? $before_title.$title.$after_title : ''; ?>
         
         <div class="fb-page" 
-            data-href="<?= !empty(rr_option_url_pagina()) ? rr_option_url_pagina():'https://www.facebook.com/facebook'; ?>" 
-            data-small-header="<?= rr_option_cabecalho_likebox();?>" 
-            <?php if(rr_option_reponsivo_likebox()=="true"){?>
-            data-adapt-container-width="<?= rr_option_reponsivo_likebox();?>" 
+            data-href="<?= !empty($data['url_pagina']) ? $data['url_pagina']:'https://www.facebook.com/facebook'; ?>" 
+            data-small-header="<?= $data['cabecalho_likebox'];?>" 
+            <?php if($data['reponsivo_likebox']=="true"){?>
+            data-adapt-container-width="<?= $data['reponsivo_likebox']; ?>" 
             <?php }else{?>
-            data-width="<?= rr_option_largura_likebox(); ?>"
-            data-height="<?=rr_option_altura_likebox(); ?>"
+            data-width="<?= $data['largura_likebox']; ?>"
+            data-height="<?= $data['altura_likebox']; ?>"
             <?php } ?>
-            data-hide-cover="<?= rr_option_capa_likebox();?>" 
-            data-show-facepile="<?= rr_option_faces_likebox();?>">
+            data-hide-cover="<?= $data['capa_likebox']; ?>" 
+            data-show-facepile="<?= $data['faces_likebox']; ?>">
            <blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore">
                <a href="https://www.facebook.com/facebook">Facebook</a>
            </blockquote>
@@ -56,7 +55,11 @@ class widget_LikeBox extends WP_Widget {
         <p>
             <label for="<?= $this->get_field_id('title'); ?>">
                 Titulo:
-                <input type="text" id="<?= $this->get_field_id('title'); ?>" name="<?= $this->get_field_name('title'); ?>" class="widefat" value="<?= $title; ?>"/> <?php _e('Exibe o Titulo do Widget'); ?>
+                <input type="text" 
+                    id="<?= $this->get_field_id('title'); ?>" 
+                    name="<?= $this->get_field_name('title'); ?>" 
+                    class="widefat" value="<?= $title; ?>"/> 
+                    <?php _e('Exibe o Titulo do Widget'); ?>
             </label>
         </p>   
     <?php
